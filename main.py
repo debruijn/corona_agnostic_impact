a5c23fe06567fa6fe15c74cd22687374e7354bad
Bert de Bruijn <4983172-bert.debruijn@users.noreply.gitlab.com>
2020-07-04 13:57:37 +0200

    First commit with first workflow setup

---
+++ main.py
@@ -0,0 +1,16 @@
+# Workflow
+
+from python import d_import_processing, d_eda, m_modeling, o_output
+
+
+def main():
+    config = 1
+
+    d_import_processing.run()  # Import files and process to pandas DF with fields as expected
+    d_eda.run()  # Visualize the data for EDA purposes
+    m_modeling.run()  # Run model (per country first)
+    o_output.run()  # Create plots based on model results
+
+
+if __name__ == "main":
+    main()
