9a7e99d322e4789d704fa307b9cc2d871557e6a0
Bert de Bruijn <4983172-bert.debruijn@users.noreply.gitlab.com>
2020-07-06 22:32:35 +0200

    First version of EDA and changes in processing needed for EDA

--- python/d_eda.py
+++ python/d_eda.py
@@ -1,2 +1,42 @@
+import pandas as pd
+
+
+def plots_by_week(data):
+
+    data.loc[data.year<2020].filter(regex="week|all_").groupby('week').mean().plot()
+
+    save_loc = 'eda/by_week/'
+
+    for col in data.columns:
+        if col not in ('year', 'week', 'nr_days'):
+            file_name = 'plot_by_week_' + col + '.png'
+            data.loc[data.year < 2020].filter(items=('week', col)).groupby('week').mean().plot().figure.\
+                savefig(save_loc + file_name)
+
+    ages = ("all", "under65", "65to80", "over80")
+
+    for col in ages:
+        file_name = 'plot_by_week_' + col + '_MF.png'
+        data.loc[data.year < 2020].filter(items=('week', col+'_M', col+'_F')).groupby('week').mean().plot().figure. \
+            savefig(save_loc + file_name)
+
+
 def run():
+
+    # Load data
+    data = pd.read_pickle('data/processed/deaths_by_full_week.pkl')
+
+    # Make plots by week, excl 2020
+    plots_by_week(data)
+
+    # Make plots by full year, excl 2020
+
+    # Make plots until week 25, incl 2020
+
     return None
+
+
+if __name__ == "__main__":
+    import os
+    os.chdir('..')
+    run()
--- python/d_import_processing.py
+++ python/d_import_processing.py
@@ -7,16 +7,37 @@ def import_data():
     return data
 
 
-def process_week_data(data):
+def process_week_data(data, full_week=False):
 
     data = data.reset_index()
-    data = pd.concat((data, data['index'].str.split(' week ', expand=True).rename(
+    data = pd.concat((data, data['index'].str.split(' week', expand=True).rename(
         columns={0: 'year', 1: 'week'})), axis=1)
     data = data.drop('index', axis=1)
+    data.year = data.year.astype('int64')
 
-    # TODO: convert data by week to 'average by day' while taking partial weeks into account
+    # Remove text from week ('22*') and days ('(2 dagen)')
+    data_weeks = data.week.str.split('(', expand=True).rename(columns={0: 'week', 1: 'nr_days'})
+    data_weeks['week'] = data_weeks['week'].str.strip().str.replace('*', '')
+    if not full_week:
+        data_weeks['nr_days'] = data_weeks.nr_days.str.split(' ', expand=True).rename(columns={0: 'nr_days'}).nr_days
+        data_weeks.loc[data_weeks.nr_days.isna(), 'nr_days'] = 7
+
+    # Add processed week and nr days back to original data
+    data['week'] = data_weeks['week'].astype('int64')
+    if not full_week:
+        data['nr_days'] = data_weeks['nr_days'].astype('int64')
+    else:
+        data['nr_days'] = 7
 
     return data
+
+
+def create_day_data_by_week(data):
+
+    day_data = data.filter(like="_").divide(data.nr_days, axis=0).drop('nr_days', axis=1)
+    day_data = day_data.combine_first(data)
+
+    return day_data
 
 
 def process_data(data):
@@ -24,9 +45,13 @@ def process_data(data):
     # Fix headers
     data = data.drop('Perioden')
     data = data.astype('int64')
-    data = data.rename(columns={'Totaal leeftijd': 'all', '0 tot 65 jaar': 'under65', '65 tot 80 jaar': '65to80',
-                         '80 jaar of ouder': 'over80', 'Mannen': 'M', 'Vrouwen': 'F',
-                         'Totaal mannen en vrouwen': 'A'})
+    data = data.rename(columns={'Totaal leeftijd': 'all',
+                                '0 tot 65 jaar': 'under65',
+                                '65 tot 80 jaar': '65to80',
+                                '80 jaar of ouder': 'over80',
+                                'Mannen': 'M',
+                                'Vrouwen': 'F',
+                                'Totaal mannen en vrouwen': 'A'})
     data.columns = ['_'.join(col) for col in data.columns]
 
     # Split year data from week data
@@ -36,7 +61,7 @@ def process_data(data):
     full_week_data = week_data.loc[[x for x in week_data.index if 'dag' not in x]]
 
     week_data = process_week_data(week_data)
-    full_week_data = process_week_data(full_week_data)
+    full_week_data = process_week_data(full_week_data, full_week=True)
 
     return {'year': year_data, 'week': week_data, 'full_week': full_week_data}
 
@@ -45,7 +70,7 @@ def check_totals(data_dict):
 
     year_data = data_dict['year']
     week_data = data_dict['week']
-    diffs = {i: week_data.loc[week_data.year == i,].all_A.sum() - year_data.loc[i].all_A.sum() for i in year_data.index}
+    diffs = {i: week_data.loc[week_data.year == int(i),].all_A.sum() - year_data.loc[i].all_A.sum() for i in year_data.index}
 
     if not all(diffs[year] == 0 for year in diffs):
         raise(ValueError, 'Year data and week data don\'t align')
@@ -60,12 +85,14 @@ def save_data(data_dict):
     data_dict['year'].to_pickle(save_dir + 'deaths_by_year.pkl')
     data_dict['week'].to_pickle(save_dir + 'deaths_by_week.pkl')
     data_dict['full_week'].to_pickle(save_dir + 'deaths_by_full_week.pkl')
+    data_dict['day'].to_pickle(save_dir + 'deaths_by_day.pkl')
 
 
 def run():
 
     data = import_data()
     data_dict = process_data(data)
+    data_dict['day'] = create_day_data_by_week(data_dict['week'])
     save_data(data_dict)
     check_totals(data_dict)
 
