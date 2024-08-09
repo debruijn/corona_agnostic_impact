20d8af10fefa2ec3463c7ee581d90ad5f19abc32
Bert de Bruijn <4983172-bert.debruijn@users.noreply.gitlab.com>
2024-08-09 19:33:39 -0400

    Changes to main/config for making this new setup work

--- config/config.py
+++ config/config.py
@@ -1,7 +1,11 @@
 data_version = ["04072020_132924", "24102020_154220b"]
 data_version = data_version[1]
 run_eda = False
-kpis = ['all_A', 'over80_A', '65to80_A', 'under65_A']
+kpis = ['all_A', 'over80_A', '65to80_A', 'under65_A'][0]
 if type(kpis) == str:
     kpis = [kpis]
-approach = ['frequentist', 'bayesian', 'machine-learning']
+packages = ['statsmodels', 'numpyro', 'tensorflow']
+
+periods = {"train_test": (2019, 8),
+           "before_covid": (2020, 8),
+           "incl_1st_wave": (2020, 16)}
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Workflow
 
-from python import d_import_processing, d_eda, m_modeling_numpyro, m_modeling_statsmodels, m_postmodeling, o_output
+from python import (d_import_processing, d_eda, m_modeling_numpyro, m_modeling_statsmodels, m_modeling_tensorflow,
+                    m_postmodeling, o_output)
 from config import config
 
 
@@ -9,10 +10,12 @@ def main():
     d_import_processing.run(config)  # Import files and process to pandas DF with fields as expected
     if config.run_eda:
         d_eda.run(config)  # Visualize the data for EDA purposes
-    if 'bayesian' in config.approach:
+    if 'numpyro' in config.packages:
         m_modeling_numpyro.run(config)  # Run model
-    if 'frequentist' in config.approach:
+    if 'statsmodels' in config.packages:
         m_modeling_statsmodels.run(config)
+    if 'tensorflow' in config.packages:
+        m_modeling_tensorflow.run(config)
     m_postmodeling.run(config)  # Visualize model results
     o_output.run(config)  # Create plots based on model results
 
